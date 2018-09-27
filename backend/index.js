e5ee155485a8bd3e8ab4ab76693a5b387d9dcc4a
Rony <rony@sti.ufpb.br>
2018-09-27 11:38:55 -0300

    Uso do cors

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,17 @@
+
+require('dotenv').config()
 const express = require('express')
 const app = express()
-
 const PORT = process.env.PORT || 3001
 const HOSTNAME = process.env.HOST || '127.0.0.1'
-
-const indexRouter = require('./routers/index.js');
-app.use('/', indexRouter);
-
-const userRouter = require('./routers/user.js');
-app.use('/users', userRouter);
-
-
+//middlewares
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ extended: true }));
+const cors = require("cors");
+app.use(cors()); //TODO - Mudar para que nao permita requisicoes de todos os servidores
+//routes
+const userRouter = require("./routers/user");
+app.use("/users", userRouter);
 var mongoose = require("mongoose");
 // mongo --host sigteste.sti.ufpb.br --port 5556 -u csiadmin -p 'c$!s3cret' --authenticationDatabase 'admin'
 mongoose.connect(
